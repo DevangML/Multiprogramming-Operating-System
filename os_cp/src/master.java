0d762f16de52090fcd9236c54b3462b1ef38cb66
Devang Manjramkar <devangmanjramkar@gmail.com>
2022-10-06 15:52:56 +0530

    created constructor

--- os_cp/src/master.java
+++ os_cp/src/master.java
@@ -1,3 +1,16 @@
 public class master {
-  
+
+  String[][] M = new String[100][4];
+  String[] IR = new String[4];
+  String[] R = new String[4];
+  int SI = 0;
+  int ic = 0;
+  String c;
+  String[][] buffer = new String[10][4];
+
+  public master(String[][] M, String[] IR, String[] R) {
+    this.M = M;
+    this.IR = IR;
+    this.R = R;
+  }
 }
