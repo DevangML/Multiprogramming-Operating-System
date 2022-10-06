ec1c9d1d7c842b5be65e21d845340f4f266c9ac9
Devang Manjramkar <devangmanjramkar@gmail.com>
2022-10-06 16:08:07 +0530

    Completed Terminate()

--- os_cp/src/master.java
+++ os_cp/src/master.java
@@ -1,3 +1,7 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.Writer;
+
 public class master {
 
   String[][] M = new String[100][4];
@@ -7,10 +11,22 @@ public class master {
   int ic = 0;
   String c;
   String[][] buffer = new String[10][4];
+  String file; // For storing file location
 
-  public master(String[][] M, String[] IR, String[] R) {
+  public master(String[][] M, String[] IR, String[] R, String file) {
     this.M = M;
     this.IR = IR;
     this.R = R;
+    this.file = file; // For getting file location from main()
   }
+
+  public void terminate() {
+    Writer output;
+    output = new BufferedWriter(new FileWriter(file, true));
+    output.append('\n');
+    output.append('\n');
+    output.close();
+    load();
+  }
+
 }
